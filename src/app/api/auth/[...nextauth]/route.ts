645e90211d36dd72ad40512ac121b8abea51fcfd
DevSnow <nicolas.benavides01@gmail.com>
2025-04-01 22:18:04 -0300

    se agrego nextauth y las politicas de inision de session

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,34 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: { label: "Usuario", type: "text" },
+        password: { label: "Contraseña", type: "password" },
+      },
+      async authorize(credentials) {
+        // Simulación (reemplaza con tu lógica real)
+        if (
+          credentials?.username === "admin" &&
+          credentials?.password === "admin"
+        ) {
+          return { id: "1", name: "Admin", email: "admin@example.com" };
+        }
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt" as const,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from '@/components/header'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <Header main='Main Page' sub='Dashboard' />
+    </div>
+  )
+}
+
+export default page
